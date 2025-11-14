2b5f043fc196e1bfff95c946df00a4c54c53168e
Your Name <you@example.com>
2025-11-14 09:43:49 +0100

    Update

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import quote from '/assets/quote1.png'
 
 const Card = () => {
   return (
-    <div className='h-full md:px-6 px-0 my-16 mx-auto '>
-        <div className='h-[400px] md:w-[1000px] md:mx-w-[300px] mx-auto bg-[#0D0630] flex flex-col justify-center items-center w-full'>
-            {/* <h1 className='text-6xl py-0 '></h1> */}
+    <div className='h-full md:px-6 px-0  mx-auto '>
+      <div className='h-[500px] flex flex-col justify-center '>
+      <div className='  md:w-[1000px]  flex flex-col justify-center text-left items-start mx-auto'>    
+         <img className=' w-[150px]   flex items-left ' src={quote} alt="" />
+         </div>
+        <div className=' md:w-[1000px] mx-auto bg-[#0D0630] flex flex-col items-center w-full '>
+     
             <h1 className='md:text-6xl text-3xl md:px-12 text-center md:text-left md:w-full w-[300px] px-2 text-[#F4D35E] overflow-y-hidden '>Having our memories turned into timeless visuals has been nothing short of magical.
 </h1>
 <p className='pt-6 md:px-12 md:text-1xl md:text-left text-center w-[300px] md:w-full text-white '>We are available for traveling within and outside Nigeria</p>
 
+        </div>
         </div>
 
     </div>
